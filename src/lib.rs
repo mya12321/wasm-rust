b41b7ebf8c594310abeaf5397fdec4b0d4c23748
Andy Miao <mya12321@gmail.com>
2020-08-03 11:36:23 +0800

    modified

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,32 @@
 use wasm_bindgen::prelude::*;
-use md2::Md2;
+use md2::{Md2, Digest};
 use md4::Md4;
 use md5::Md5;
+use hex;
 
 #[wasm_bindgen]
-pub fn md2(input: &str) -> &str {
+pub fn calcmd2(input: &str) {
+    let mut s = String::from(input);
     for i in 0..100000000 {
-      input = Md2.digest();
+      s = hex::encode(Md2::digest(&s.into_bytes()));
     }
-    println("{}", input);
+    println!("{}", &s);
 }
 
 #[wasm_bindgen]
-pub fn md4(input: &str) -> &str {
+pub fn calcmd4(input: &str) {
+    let mut s = String::from(input);
     for i in 0..100000000 {
-      input = Md2.digest();
+      s = hex::encode(Md4::digest(&s.into_bytes()));
     }
-    println("{}", input);
+    println!("{}", &s);
 }
 
 #[wasm_bindgen]
-pub fn md5(input: &str) -> &str {
+pub fn calcmd5(input: &str) {
+    let mut s = String::from(input);
     for i in 0..100000000 {
-      input = Md2.digest();
+      s = hex::encode(Md5::digest(&s.into_bytes()));
     }
-    println("{}", input);
+    println!("{}", &s);
 }
